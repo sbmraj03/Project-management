aff1d1d8b10e0236025877ac638a176c51951959
sbmraj03 <sumbamraj3@gmail.com>
2025-09-20 13:54:41 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // Important for deployment: set base to '/' or your subpath
-  base: '/', // Change to '/my-app/' if deployed under a subfolder
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
