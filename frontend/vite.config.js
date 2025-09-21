8c5dd348dc0544b4ac83ba8c09ee90c0ad458f11
sbmraj03 <sumbamraj3@gmail.com>
2025-09-21 11:02:01 +0530

    made some changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,20 @@ export default defineConfig({
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom'],
           charts: ['chart.js', 'react-chartjs-2']
-        }
+        },
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/images/[name]-[hash][extname]`
+          }
+          if (/css/i.test(ext)) {
+            return `assets/css/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
       }
     }
   },
